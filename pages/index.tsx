b887e8e837f12af5f2264a0307c879c502923954
Noor Shabib <noor@simplify9.com>
2022-09-14 23:26:03 +0300

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default class Home extends Component {
     return (
       <>
         <Head>
-          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" async></script>
         </Head>
         <article>
           <h1>{title}</h1>
